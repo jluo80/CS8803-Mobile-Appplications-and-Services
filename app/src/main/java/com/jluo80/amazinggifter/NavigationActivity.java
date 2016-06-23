9c86dcecf80f14f607e66fe6e1f5078283164158
jluo80 <jluo80@gatech.edu>
2016-06-22 23:26:45 -0400

    navigation bar & swipe gesture

--- app/src/main/java/com/jluo80/amazinggifter/NavigationActivity.java
+++ app/src/main/java/com/jluo80/amazinggifter/NavigationActivity.java
@@ -1,7 +1,9 @@
 package com.jluo80.amazinggifter;
 
+import android.content.Intent;
 import android.support.design.widget.CoordinatorLayout;
 import android.support.design.widget.Snackbar;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -16,21 +18,23 @@ public class NavigationActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_navigation);
 
-        coordinatorLayout = (CoordinatorLayout) findViewById(R.id.three_buttons_activity);
+        coordinatorLayout = (CoordinatorLayout) findViewById(R.id.navigation_activity);
 
         BottomBar bottomBar = BottomBar.attach(this, savedInstanceState);
         bottomBar.setItemsFromMenu(R.menu.navigation_menu, new OnMenuTabSelectedListener() {
             @Override
             public void onMenuItemSelected(int itemId) {
                 switch (itemId) {
-                    case R.id.recent_item:
-                        Snackbar.make(coordinatorLayout, "Recent Item Selected", Snackbar.LENGTH_LONG).show();
+                    case R.id.my_gifts_item:
+                        Intent intent = new Intent(NavigationActivity.this, AllGiftsList.class);
+                        startActivity(intent);
+//                        Snackbar.make(coordinatorLayout, "My Gifts Item Selected", Snackbar.LENGTH_LONG).show();
+//                        break;
+                    case R.id.friends_item:
+                        Snackbar.make(coordinatorLayout, "Friends Item Selected", Snackbar.LENGTH_LONG).show();
                         break;
-                    case R.id.favorite_item:
-                        Snackbar.make(coordinatorLayout, "Favorite Item Selected", Snackbar.LENGTH_LONG).show();
-                        break;
-                    case R.id.location_item:
-                        Snackbar.make(coordinatorLayout, "Location Item Selected", Snackbar.LENGTH_LONG).show();
+                    case R.id.about_me_item:
+                        Snackbar.make(coordinatorLayout, "About Me Item Selected", Snackbar.LENGTH_LONG).show();
                         break;
                 }
             }
@@ -48,5 +52,15 @@ public class NavigationActivity extends AppCompatActivity {
         // Use custom typeface that's located at the "/src/main/assets" directory. If using with
         // custom text appearance, set the text appearance first.
         //bottomBar.setTypeFace("MyFont.ttf");
+
+        // Find the view pager that will allow the user to swipe between fragments
+        ViewPager viewPager = (ViewPager) findViewById(R.id.viewpager);
+
+        // Create an adapter that knows which fragment should be shown on each page
+        SimpleFragmentPagerAdapter adapter = new SimpleFragmentPagerAdapter(getSupportFragmentManager());
+
+        // Set the adapter onto the view pager
+        viewPager.setAdapter(adapter);
+
     }
 }
