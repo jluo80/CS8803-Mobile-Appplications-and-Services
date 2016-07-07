05a31e374ad19192fc2e1bd4b902e7e99e583cdf
jluo80 <jluo80@gatech.edu>
2016-07-07 05:06:46 -0400

    Search from Amazon view and Asynctak image loading

---
+++ app/src/main/java/com/jluo80/amazinggifter/Gift.java
@@ -0,0 +1,62 @@
+package com.jluo80.amazinggifter;
+
+/**
+ * Created by Jiahao on 7/2/2016.
+ */
+public class Gift {
+    String itemId;
+    String itemTitle;
+    String itemUrl;
+    String galleryUrl;
+    String currentPrice;
+
+    public Gift() {}
+
+    public Gift(String itemId, String itemTitle, String currentPrice, String galleryUrl, String itemUrl) {
+        this.itemId = itemId;
+        this.itemTitle = itemTitle;
+        this.currentPrice = currentPrice;
+        this.galleryUrl = galleryUrl;
+        this.itemUrl = itemUrl;
+    }
+
+    public String getItemId() {
+        return itemId;
+    }
+
+    public void setItemId(String itemId) {
+        this.itemId = itemId;
+    }
+
+    public String getItemTitle(){
+        return itemTitle;
+    }
+
+    public void setItemTitle(String itemTitle){
+        this.itemTitle = itemTitle;
+    }
+
+    public String getCurrentPrice(){
+        return currentPrice;
+    }
+
+    public void setCurrentPrice(String currentPrice){
+        this.currentPrice = currentPrice;
+    }
+
+    public String getGalleryUrl(){
+        return galleryUrl;
+    }
+
+    public void setGalleryUrl(String galleryUrl){
+        this.galleryUrl = galleryUrl;
+    }
+
+    public String getItemUrl(){
+        return itemUrl;
+    }
+
+    public void setItemUrl(String itemUrl){
+        this.itemUrl = itemUrl;
+    }
+}
